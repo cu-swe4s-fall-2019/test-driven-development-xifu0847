f3c74f6daf43afa6b0f406be2f78a2c4b09d1704
Xinyi Fu <xifu0847@colorado.edu>
2019-09-26 17:41:53 +0000

    Update math_lib.py and add test_math_lib.py
    Update math_lib.py with list_mean and list_stdev methods.
    The methods will raise an expection if the input is empty.
    Add unit test for math_lib.py.
    The unit test has four testsets: empty_list, int_static_list,
    float_static_list and random_list
    
    Signed-off-by: Xinyi Fu <xifu0847@colorado.edu>

--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,18 @@
+import math
 
-def list_mean(L):
-    return None
+def list_mean(V):
+	mean = None
+	try:
+		mean = sum(V) / len(V)
+	except ZeroDivisionError as error:
+		raise ZeroDivisionError('ZeroDivisionError: V has len 0')
+	return mean
 
-def list_stdev(L):
-    return None
+def list_stdev(V):
+	mean = list_mean(V)
+	stdev = None
+	try:
+		stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / (len(V) - 1))
+	except ZeroDivisionError as error:
+		raise ZeroDivisionError('ZeroDivisionError: V has len 0')
+	return stdev
